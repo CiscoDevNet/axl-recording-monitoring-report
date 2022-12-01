5743edbc7e17473d08630ed37267f255f6594cf6
dstaudt <dstaudt@cisco.com>
2022-12-01 17:27:03 -0600

    Before using AXL

--- report.py
+++ report.py
@@ -3,10 +3,10 @@ import untangle
 check = '\u2713'
 
 excludeList = [
-    'Universal Device Template',
-    'IPTrade Profile EK',
-    'IPTrade TAD',
-    'Nokia S60'
+    'Universal Device Template', # N/A
+    'Cisco TelePresence', # Not a real/configurable device?
+    'Nokia S60', # 3rd party
+    'Cisco 7910' # Deprecated as of 11.5(1)
 ]
 report = untangle.parse( 'UnifiedCMPhoneFeatureList.xml' )
 
@@ -19,20 +19,23 @@ supportedList = {}
 for device in featureList:
 
     if device.cell[ 0 ] in excludeList: continue
+    if device.cell[ 1 ] == 'SCCP': continue
     
-    if device.cell[2] in ('Record','Monitor','Built In Bridge'):
+    if device.cell[ 2 ] in ( 'Record', 'Monitor', 'Built In Bridge' ):
 
-        deviceName = device.cell[0].cdata + ' (' + device.cell[ 1 ].cdata + ')'
+        deviceName = device.cell[0].cdata
 
         if not deviceName in supportedList:
 
             supportedList[ deviceName ] = { 'Record': False, 'Monitor': False, 'Built In Bridge': False }
 
-        supportedList[ deviceName ].update( { device.cell[2].cdata: True } )
+        supportedList[ deviceName ].update( { device.cell[ 2 ].cdata: True } )
+
+supportedList = dict( sorted( supportedList.items() ) )
 
 outFile = open( 'supported_list.md', 'w' )
 
-outFile.write( 'Device/Phone (Protocol) | Monitoring | Gateway Recording | Phone/BiB Recording\n' )
+outFile.write( 'Device/Phone (Protocol) | Monitoring | Gateway Recording | Phone (BiB) Recording\n' )
 outFile.write( '----------------------- | ---------- | ----------------- | -------------------\n' )
 
 for deviceName, features in supportedList.items():
